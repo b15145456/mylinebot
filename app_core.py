22e4c252412f502c5d129dbeea0e7c6f17df697e
張祐綸 <>
2021-06-28 09:31:01 +0800

    fix MessageEvent

--- app_core.py
+++ app_core.py
@@ -1,20 +1,21 @@
-# 載入需要的模組
 from __future__ import unicode_literals
 import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 import configparser
+
+app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-app = Flask(__name__)
-
-# LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -34,9 +35,15 @@ def callback():
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )  
+    
+    # 這次我加了下面這一行
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+    # 這次我加了上面這一行
+    
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
 if __name__ == "__main__":
     app.run()
