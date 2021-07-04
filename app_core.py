ea6cc22353addc1c6e2160d32d62c767292b0b51
張祐綸 <>
2021-07-04 15:53:44 +0800

    add basic web page

--- app_core.py
+++ app_core.py
@@ -1,11 +1,18 @@
 from __future__ import unicode_literals
 import os
-from flask import Flask, request, abort
+
+# 增加了 render_template
+from flask import Flask, request, abort, render_template
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 import configparser
+
+import urllib
+import re
+import random
 
 app = Flask(__name__)
 
@@ -16,6 +23,10 @@ config.read('config.ini')
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+# 增加的這段放在上面
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -32,18 +43,44 @@ def callback():
 
     return 'OK'
 
-# 學你說話
+# 請 pixabay 幫我們找圖
 @handler.add(MessageEvent, message=TextMessage)
-def echo(event):
+def pixabay_isch(event):
     
-    # 這次我加了下面這一行
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-    # 這次我加了上面這一行
-    
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+        try:
+            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+            
+            req = urllib.request.Request(url, headers = headers)
+            conn = urllib.request.urlopen(req)
+            
+            print('fetch page finish')
+            
+            pattern = 'img srcset="\S*\s\w*,'
+            img_list = []
+            
+            for match in re.finditer(pattern, str(conn.read())):
+                img_list.append(match.group()[12:-3])
+                
+            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+            print('fetch img url finish')
+            print(random_img_url)
+            
+            line_bot_api.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url=random_img_url,
+                    preview_image_url=random_img_url
+                )
+            )
+        # 如果找不到圖，就學你說話
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text)
+            )
+            pass
 
 if __name__ == "__main__":
     app.run()
