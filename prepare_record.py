d401a65ae04c9a1ebc03d0ace963d5a5de9b75ad
DESKTOP-O0JIHCH\ulun <b15145456@gmail.com>
2021-07-07 17:15:49 +0800

    function fix for insert data

--- prepare_record.py
+++ prepare_record.py
@@ -2,18 +2,15 @@ import datetime
 
 def prepare_record(text):
     text_list = text.split('\n')
-    
     month = text_list[0].split(' ')[0].split('/')[0]
     day = text_list[0].split(' ')[0].split('/')[1]
     d = datetime.date(datetime.date.today().year, int(month), int(day))
-   
     record_list = []
     
     time_format = '%H:%M'
     
     for i in text_list[1:]:
         temp_list = i.split(' ')
-        
         temp_name = temp_list[0]
         temp_training = temp_list[1]
         
@@ -22,6 +19,6 @@ def prepare_record(text):
         temp_duration = temp_end - temp_start
         
         record = (temp_name, temp_training, temp_duration, d)
+        print(record)
         record_list.append(record)
-        
     return record_list
