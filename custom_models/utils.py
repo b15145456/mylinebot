7dbf24ce9f755c963c06dc6c154b42bd41eef4d3
DESKTOP-O0JIHCH\ulun <b15145456@gmail.com>
2021-08-04 13:49:38 +0800

    no message

--- custom_models/utils.py
+++ custom_models/utils.py
@@ -1,22 +1,20 @@
 import os
 import psycopg2
 
-def edit_number(record_list):
+def edit_number(change_num):
     DATABASE_URL = os.environ['DATABASE_URL']
     # DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a clinic-bot-v1').read()[:-1]
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-
+    data = ["0",str(change_num)]
     table_columns = '(departments_id, number, edit_time)'
     postgres_insert_query = f"""UPDATE clinic_number {table_columns} VALUES (%s,%s,CURRENT_TIMESTAMP)"""
 
-
-
-    cursor.executemany(postgres_insert_query, record_list)
+    cursor.executemany(postgres_insert_query, data)
     conn.commit()
 
-    message = f"恭喜您！ {cursor.rowcount} 筆資料成功匯入 clinic_number 表單！success insert data!!"
+    message = f"恭喜您！ 成功修改資料"
 
     cursor.close()
     conn.close()
