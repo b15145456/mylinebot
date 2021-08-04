517faba5ccf0adf41b042a8464bf71f4e2c6d5e2
chang yulun <b15145456@gmail.com>
2021-08-05 01:32:50 +0800

    no message

--- custom_models/utils.py
+++ custom_models/utils.py
@@ -8,7 +8,7 @@ def edit_number(change_num):
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
     data = change_num
-    postgres_insert_query = f"""UPDATE clinic_number SET departments_id=0, now_number={data}, edit_time=CURRENT_TIMESTAMP;"""
+    postgres_insert_query = f"""UPDATE clinic_number SET departments_id=0, number_now={data}, edit_time=CURRENT_TIMESTAMP;"""
 
     cursor.execute(postgres_insert_query)
     conn.commit()
@@ -53,7 +53,7 @@ def reset_number():
     cursor = conn.cursor()
     data = 0
     table_columns = '(departments_id, number, edit_time)'
-    postgres_insert_query = f"""UPDATE clinic_number SET departments_id=0, now_number={data}, edit_time=CURRENT_TIMESTAMP;"""
+    postgres_insert_query = f"""UPDATE clinic_number SET departments_id=0, number_now={data}, edit_time=CURRENT_TIMESTAMP;"""
 
     cursor.executemany(postgres_insert_query, data)
     conn.commit()
